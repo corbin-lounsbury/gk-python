df45d4e560a549757c939a1c21ed05635ac3f6b8
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-14 11:34:29 -0500

    Refactor to use floats for dollar fractions

--- lab2.py
+++ lab2.py
@@ -3,9 +3,9 @@
 # 2. Input the hourly rate and number of hours worked
 # 3. Input a 40-hour work week at $60/hour
 
-payRate = int(input("How much money per hour do you earn? : $"))
-payHours = int(input("How many hours did you work? : "))
+payRate = float(input("How much money per hour do you earn? : $"))
+payHours = float(input("How many hours did you work? : "))
 
 totalRate = payRate*payHours
 
-print(f"You made ${totalRate} by working {payHours} hours and earning ${payRate} per hour")
+print(f"You made ${totalRate:.2f} by working {payHours} hours and earning ${payRate:.2f} per hour")
