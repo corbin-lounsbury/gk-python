4d268ec2f178372399a6e5789e942b4af6529d84
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-14 10:52:29 -0500

    Add comments, instructions

--- lab1.py
+++ lab1.py
@@ -1,3 +1,10 @@
+# Instructions
+# 1. Create a List, Tuple, Set, and Dictionary
+# 2. Print each type
+# 3. Confirm each type of data structures you created
+# 4. Try adding an item to each type
+# 5. Print each type again
+
 myList = [1,2,3]
 myTuple = (1,2,3)
 myDict = {"1":"one", "2":"two", "3":"three" }
@@ -17,6 +24,7 @@ print(mySet)
 
 print("Adding data")
 myList.append(4)
+# skipped attempting to add to tuple, it is an immutable data structure
 myDict["4"]="four"
 mySet.add(4)
 
