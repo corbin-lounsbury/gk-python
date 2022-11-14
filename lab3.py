b95b79666413f62aefa4abf84bc2b6056aaf3ef3
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-14 13:27:47 -0500

    Add instructions

--- lab3.py
+++ lab3.py
@@ -1,3 +1,11 @@
+# Instructions
+# 1. Create an if statement that checks whether a user is logged in and is a Gold Member
+# 2. If the user is logged in, print "User is logged in".
+# 3. If the user is lagged in and is a Gold Member, print "Welcome Gold Member!".
+# 4. If the user is not logged in and is a Gold Member, print "Please log in, Gold Member".
+# 5. If the user is not logged in, print "Please log in".
+# 6. Test for the user that is not logged in and is a Gold Member.
+
 isLoggedIn = False
 isGoldMember = True
 
