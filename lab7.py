b61b06f95714e3e807b381a306f7692bf9440929
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-15 12:04:45 -0500

    Simplified using the sub() method

--- lab7.py
+++ lab7.py
@@ -7,21 +7,6 @@ import re
 
 oneString = input("Please enter a series of 1's")
 
-ones = re.findall(r"1", oneString)
+ones = re.sub(r"1", "one", oneString)
 
-newOnes = []
-oneCount = 0
-
-for i, one in enumerate(ones):
-    if one == "1":
-        print("Found a 1, converting to one")
-        newOnes.append("one")
-        oneCount +=1
-    else:
-        print(f"Not a 1, leavings as is")
-        newOnes.append(one)
-
-print(f"I found {oneCount} instances of the number 1")
-
-result = "".join(newOnes)
-print(result)
+print(ones)
