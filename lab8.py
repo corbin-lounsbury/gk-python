f47e9d104df6de97d9842b53c3b71e94cd1e634c
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-15 14:20:45 -0500

    Add instructions

--- lab8.py
+++ lab8.py
@@ -1,3 +1,8 @@
+# Instructions
+# 1. Create a lambda expression that prints out your argument and your argument added by 2
+#   a. Use a variable that holds the lambda expression
+#   b. Print the results
+
 inArg = int(input("input a number "))
 
 lambVar = lambda: print (f"{inArg} and {(inArg+2)}")
