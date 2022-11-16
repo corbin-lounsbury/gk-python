40a6aad7970f8cebacf0de39144840396c64a681
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-16 10:06:26 -0500

    initial commit

---
+++ lab10.py
@@ -0,0 +1,38 @@
+# Instructions
+
+# 1. Create the class Car
+
+# 2. Add a class/static variable
+
+# 3. Create a class/static attributes
+
+# 4. Create a class method
+
+# 5. create a static method using @staticmethod
+
+# 6. Instantiate an object
+
+# 7. Print the static variable, class method, and static method of an object
+
+
+class Car:
+    def __init__(self, make, model, year, color, msrp):
+        self.make = make
+        self.model = model
+        self.year = int(year)
+        self.color = color
+        self.msrp = float(msrp)
+
+    def yearMade(self):
+        print (f"This care was made in {self.year}")
+
+    @staticmethod
+    def calculateAge(thisYear, carYear):
+        print(f"This car is {(thisYear-carYear)} years old")
+
+
+thisCar = Car("Ford", "F150", 2019, "white", 60000)
+
+thisCar.yearMade()
+
+thisCar.calculateAge(2022, thisCar.year)
