eac455cdab543f3ddea939819a0c331e2b947a46
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-16 13:21:51 -0500

    Add instructions

--- lab12.py
+++ lab12.py
@@ -1,3 +1,11 @@
+# Instructions
+
+# 1. Create a local method
+
+# 2. Attempt to change the local() method
+
+# 3. Print the results
+
 def powerHouse():
     inVar = "Mitochondria"
     print(f"{inVar} is the powerhouse of the cell")
