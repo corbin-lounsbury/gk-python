7e38b34bf52c2eaaa62850bc8bcb36260a6902f4
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-17 09:00:51 -0500

    Add else statement

--- mod3-misclabs/file.py
+++ mod3-misclabs/file.py
@@ -1,3 +1,8 @@
+# Instructions
+# 1. Write a program to look through each linke in the file. 
+# 2. Create a file
+# 3. Check if any line starts with "hi", if so, print hello in the console, else print the line
+
 import os
 import fileinput
 
@@ -6,4 +11,6 @@ testFile = currentDir + "\\mod3-misclabs\\test.txt"
 
 for line in fileinput.input(files=testFile):
     if line.startswith("hi"):
+        print("Hello")
+    else:
         print(line)
