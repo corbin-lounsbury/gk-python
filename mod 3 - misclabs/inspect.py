7e19814291394500c07ac1c649899578b5dfd506
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-17 08:45:28 -0500

    Add instructions

--- mod 3 - misclabs/inspect.py
+++ mod 3 - misclabs/inspect.py
@@ -1,5 +1,10 @@
-import inspect
+# Instructions
+# 1. Write a program to implement methods from the inspect module.
+# 2. Create a class, function, and import a module 
+# 3. use different methods from the inspect module to check for class, method in the class, function, module, and built-in method from the module.
+# 4. Print the resulting values
 
+import inspect
 import os
 
 class Customer:
@@ -29,4 +34,4 @@ customer1 = Customer("Corbin", 32, "Janitor", 8675309)
 
 customer2 = Customer("Mike", 57, "CPA", 687564)
 
-inspectThings(os,Customer,Customer.printStats)
+inspectThings(os,Customer,Customer.printStats,os.chmod)
