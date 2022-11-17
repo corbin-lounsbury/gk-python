90010b17d6926abb85ad6f8e6d081d3f831ff6fd
Corbin Lounsbury <corbin.lounsbury@gmail.com>
2022-11-17 11:32:39 -0500

    Add instructions

--- mod4-lab/counterlab.py
+++ mod4-lab/counterlab.py
@@ -1,5 +1,9 @@
 # Instructions
-# Use Counter on your name
+# 1. Write a program to count unique objects in a dataset using different ways
+# 2. Use for loop, counter variable, and get method
+# 3. Use for loop and counter variable
+# 4. Use defaultdict
+# 5. finally import counter method from collections module to countt unique objects
 
 name = str(input("Please enter your name: "))
 
